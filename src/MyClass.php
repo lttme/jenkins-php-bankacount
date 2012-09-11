04cda82b64c748e5f6e5d07f6063894ecc77ca41
yanguanglan <lanyg.com@gmail.com>
2012-09-11 21:59:46 +0800

    add pear standard

--- src/MyClass.php
+++ src/MyClass.php
@@ -1,9 +1,26 @@
 <?php
+/**
+ * @category Bankacount
+ * @package MyClass
+ * @author YanguangLan <lanyg.com@gmail.com>
+ */
+
+/**
+ * @category Bankacount
+ * @package MyClass
+ */
 class MyClass {
+    //{{{ demo
+
+    /**
+     * @param int $a
+     * @access public
+     */
     public function demo($a) {
         if($a == 1) {
             return 1;
         }
         return 0;
     }
+    //}}}
 }
